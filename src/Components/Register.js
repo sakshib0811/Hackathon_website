c5ea701f1b8a029840e822acf5e0ed502619b7d3
sakshib0811 <85578492+sakshib0811@users.noreply.github.com>
2022-04-09 14:36:13 +0530

    signup page added

--- src/Components/Register.js
+++ src/Components/Register.js
@@ -1,8 +1,72 @@
 import React from 'react'
-
+import "./Register.css"
+import register from './images/register2.png'
+import {FaUser} from 'react-icons/fa'
+import {MdEmail, MdOutlineWork} from 'react-icons/md'
+import {CgPhone} from 'react-icons/cg'
+import {RiLockPasswordFill} from 'react-icons/ri'
+import Nav from './Nav'
 function Register() {
   return (
-    <div>Register</div>
+    <div className='top'>
+      <Nav />
+      <section className='signup'>
+        <div className="container mt-5">
+          <div className="signup-content">
+            <div className="signupform">
+              <h2 className='form-title'>Register</h2>
+              <form className='register-form' id='register-form'>
+                <div className="form-group">
+                  <label htmlFor="name">
+                    <FaUser />
+                  </label>
+                  <input type="text" name='name' id='name' autoComplete='off' placeholder='Your Name' />
+                </div>
+              </form><br />
+
+              <form className='register-form' id='register-form'>
+                <div className="form-group">
+                  <label htmlFor="name">
+                    <MdEmail />
+                  </label>
+                  <input type="email" name='name' id='name' autoComplete='off' placeholder='Your Email' />
+                </div>
+              </form><br />
+
+              <form className='register-form' id='register-form'>
+                <div className="form-group">
+                  <label htmlFor="name">
+                    <CgPhone />
+                  </label>
+                  <input type="number" name='name' id='name' autoComplete='off' placeholder='Mobile Number' />
+                </div>
+              </form><br />
+
+              <form className='register-form' id='register-form'>
+                <div className="form-group">
+                  <label htmlFor="name">
+                    <MdOutlineWork />
+                  </label>
+                  <input type="text" name='name' id='name' autoComplete='off' placeholder='Your Profession' />
+                </div>
+              </form><br />
+
+              <form className='register-form' id='register-form'>
+                <div className="form-group">
+                  <label htmlFor="name">
+                    <RiLockPasswordFill />
+                  </label>
+                  <input type="password" name='name' id='name' autoComplete='off' placeholder='Password' />
+                </div>
+              </form><br />
+              <button className='btn btn1'>Register</button>
+
+            </div>
+            <img src={register} alt="" className='reg_img' />
+          </div>
+        </div>
+      </section>
+    </div>
   )
 }
 
